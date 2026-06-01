bf85649b6a863bdbb12594634b00639d7f001cf9
Gabriela Restani <git@midorirestani.com>
2026-06-01 20:24:36 -0300

    fix: enhance UiContainerProps to include class and id for improved customization

--- src/lib/components/ui/ui-container.svelte
+++ src/lib/components/ui/ui-container.svelte
@@ -4,11 +4,17 @@
   type UiContainerProps = {
     tag?: 'div' | 'section' | 'main';
     children: Snippet;
+    class?: string;
+    id?: string;
   };
 
-  let { tag = 'div', children }: UiContainerProps = $props();
+  let { tag = 'div', children, class: className, id }: UiContainerProps = $props();
 </script>
 
-<svelte:element this={tag} class="mx-auto my-0 w-full max-w-7xl py-6 lg:py-12">
+<svelte:element
+  this={tag}
+  {id}
+  class={`mx-auto my-0 w-full max-w-7xl py-6 lg:py-12 ${className || ''}`}
+>
   {@render children()}
 </svelte:element>
