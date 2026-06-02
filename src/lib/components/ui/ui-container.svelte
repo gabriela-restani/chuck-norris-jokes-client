bb6ecf7406728131d6678a7c73d76f61660a779b
Gabriela Restani <git@midorirestani.com>
2026-06-02 17:59:16 -0300

    feat: improve code formatting and structure in ui-container component

--- src/lib/components/ui/ui-container.svelte
+++ src/lib/components/ui/ui-container.svelte
@@ -9,14 +9,20 @@
     'aria-labelledby'?: string;
   };
 
-  let { tag = 'div', children, class: className, id, 'aria-labelledby': ariaLabelledby }: UiContainerProps = $props();
+  let {
+    tag = 'div',
+    children,
+    class: className,
+    id,
+    'aria-labelledby': ariaLabelledby,
+  }: UiContainerProps = $props();
 </script>
 
 <svelte:element
   this={tag}
   {id}
   aria-labelledby={ariaLabelledby}
-  class={`max-h mx-auto my-0 w-full max-w-[calc(100dvw-2rem)] py-6 md:max-w-2xl lg:max-w-4xl lg:py-12 xl:max-w-5xl ${className || ''}`}
+  class={`max-h mx-auto my-0 w-full max-w-[calc(100dvw-2rem)] md:max-w-2xl lg:max-w-4xl xl:max-w-5xl ${className || ''}`}
 >
   {@render children()}
 </svelte:element>
