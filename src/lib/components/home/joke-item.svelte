10a1bf9078640a54715c70c28701d737bd7769a4
Gabriela Restani <git@midorirestani.com>
2026-06-02 21:49:06 -0300

    feat: add highlighting functionality to joke text based on search query

--- src/lib/components/home/joke-item.svelte
+++ src/lib/components/home/joke-item.svelte
@@ -4,13 +4,35 @@
   import { shareLink } from '$lib/utils/share';
   import UiText from '$lib/components/ui/ui-text.svelte';
 
-  type CategoryJokeItemProps = { joke: Joke };
-  let { joke }: CategoryJokeItemProps = $props();
+  type CategoryJokeItemProps = { joke: Joke; query?: string };
+  let { joke, query }: CategoryJokeItemProps = $props();
+
+  const textWithHighlight = (
+    text: string,
+    highlight?: string,
+  ): Array<{ text: string; isHighlight: boolean }> => {
+    if (!highlight) return [{ text, isHighlight: false }];
+
+    const regex = new RegExp(`(${highlight})`, 'gi');
+    return text.split(regex).map((part) => ({
+      text: part,
+      isHighlight: part.toLowerCase() === highlight.toLowerCase(),
+    }));
+  };
 </script>
 
 <li class="rounded-lg border border-deep-blue-300 bg-deep-blue-800 p-5">
   <UiText tag="p" size="lg" class="text-base leading-relaxed text-cream-100">
-    {joke.value}
+    {#each textWithHighlight(joke.value, query) as part, i (i)}
+      {#if part.isHighlight}
+        <mark
+          class="bg-transparent text-cream-100 underline decoration-leather-brown-600 decoration-2 underline-offset-2"
+          >{part.text}</mark
+        >
+      {:else}
+        {part.text}
+      {/if}
+    {/each}
   </UiText>
 
   <hr class="my-3 border-deep-blue-600" />
